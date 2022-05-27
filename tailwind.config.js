1451f8801de467ac0b2c32985d8321955dc0ba8f
MikevPeeren <mikevpeeren@hotmail.com>
2022-05-27 14:51:17 +0200

    chore: init style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      gradientFrom: "hsl(6, 100 %, 80 %)",
+      gradientTo: "hsl(335, 100%, 65%)",
+      paleBlue: "hsl(243, 100%, 93%)",
+      grayBlue: "hsl(229, 7%, 55%)",
+      darkBlue: "hsl(228, 56%, 26%)",
+      veryDarkBlue: "hsl(229, 57%, 11%)",
+    },
     extend: {},
   },
   variants: {
